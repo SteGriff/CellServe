657732862633e3d6249ce5c478bf54b3a976641b
Stephen Griffiths <github@stegriff.co.uk>
2019-10-18 20:20:34 +0000

    Init

---
+++ CellServe.Service/CellServe.Web/Controllers/DataController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CellServe.Web.Controllers
+{
+    public class DataController : Controller
+    {
+        [HttpGet, ActionName("Index")]        
+        public ActionResult GetData(string table)
+        {
+            object req = new
+            {
+                Table = table,
+                Operation = "Read"
+            };
+            return Json(req, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost, ActionName("Index")]
+        public ActionResult PostData(string table, FormCollection form)
+        {
+            //var formData = form.AllKeys.Select(k => )
+
+            object req = new
+            {
+                Table = table,
+                Operation = "Write",
+                Values = form
+            };
+            return Json(req);
+        }
+    }
+}
---
+++ CellServe.Service/CellServe.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CellServe.Web.MvcApplication" Language="C#" %>
